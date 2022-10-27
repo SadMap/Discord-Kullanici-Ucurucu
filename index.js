725982aee61ab3896f6b88f3ba2915fba5babcc8
Speretta <speretta@protonmail.com>
2022-10-27 22:41:58 +0300

    Added Multiple Language System

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const TOKEN = "TOKEN";
+
+const language = 'tr';
+
+const texts = {
+  "tr": {
+    "ready": "{0} kullanıcı adıyla giriş yapıldı.",
+    "command": "?kontrol",
+    "kickreason": "Bu hesap 7 günden daha yeni!",
+    "kickbroadcast": "✅ <@{0}> | {1} sunucudan atıldı!"
+  },
+  "en": {
+    "ready": "Logged in with username {0}.",
+    "command": "?control",
+    "kickreason": "This account is newer than 7 days!",
+    "kickbroadcast": "✅ <@{0}> | {1} kicked from server!"
+  }
+};
+
 client.on('ready', () => {
-  console.log(`${client.user.tag} Kullanıcı adıyla giriş yapıldı.`);
+  console.log(texts[language]["ready"].replace("{0}", `${client.user.tag}`));
 });
 const cid ="" // Kanal idsi
 client.on('message', msg => {
-  if (msg.content === '?kontrol') {
+  if (msg.content === texts[language][command]) {
     if (msg.channel.id == cid) {
       msg.guild.members.fetch()
       .then(m => {
@@ -14,12 +33,12 @@ client.on('message', msg => {
         const re = m.filter(user => Date.now() - user.user.createdTimestamp  < 604800000)
         msg.channel.send(re.size)
         re.forEach(y => {
-          y.kick({reasson:"Bu hesap 7 günden daha yeni!"})
-          msg.channel.send(`✅ <@${y.id}> | ${y.user.tag} Sunucudan atıldı!`)
+          y.kick({reasson:texts[language][kickreason]})
+          msg.channel.send(texts[language][kickbroadcast].replace("{0}", `${y.id}`).replace("{1}", `${y.user.tag}`))
         })
       })
     }
   }
 });
 
-client.login('Token');
+client.login('TOKEN');
